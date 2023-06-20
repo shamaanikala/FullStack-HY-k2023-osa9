e43769b33ee4c86e794404cdc2a64ca592f935f5
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-06-20 18:59:40 +0300

    9.2 calculate average

--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -18,7 +18,13 @@ const dummyResult = {
   average: 1.9285714285714286
 };
 
+const avg = (array: number[]): number => {
+  const sum = array.reduce((sum, elem) => sum += elem, 0);
+  return sum/array.length;
+}
 const calculateExercises = (dailyExerciseHours: number[], targetHours: number): Result => {
+  const average = avg(dailyExerciseHours)
+  console.log(average)
   return dummyResult;
 }
 
