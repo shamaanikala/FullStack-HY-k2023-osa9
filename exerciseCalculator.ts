e9991a1f1c0debac8c2d87bccf9dd03cca62343f
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-06-20 19:35:59 +0300

    Ex9.2 done

--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -1,36 +1,60 @@
+// assuming integer rating
+type Rating = 1 | 2 | 3;
+
+ 
 interface Result {
   periodLength: number,
   trainingDays: number,
   success: boolean,
-  rating: number,
+  rating: Rating,
   ratingDescription: string,
   target: number,
   average: number
-};
-
-const dummyResult = {
-  periodLength: 7,
-  trainingDays: 5,
-  success: false,
-  rating: 2,
-  ratingDescription: 'not too bad but could do better',
-  target: 2,
-  average: 1.9285714285714286
 };
 
 const avg = (array: number[]): number => {
   const sum = array.reduce((sum, elem) => sum += elem, 0);
   return sum/array.length;
 }
+
+const rateExerciseHours = (averageHours: number, targetHours: number): Rating => {
+  if (averageHours >= targetHours) {
+    return 3;
+  } else if (averageHours >= 0.5*targetHours) {
+    return 2;
+  } else {
+    return 1;
+  }
+}
+
+const RatingDescription: string[] = [
+  'the target is still quite far away',
+  'not too bad but could be better',
+  'target reached, well done!'
+];
+ 
+const getRatingDescription = (rating: Rating): string => {
+  return RatingDescription[rating-1];
+}
+
 const calculateExercises = (dailyExerciseHours: number[], targetHours: number): Result => {
   const periodLength = dailyExerciseHours.length;
   const trainingDays = dailyExerciseHours.filter(hours => hours > 0).length;
   const average = avg(dailyExerciseHours);
+  const success = average >= targetHours;
+  const rating = rateExerciseHours(average, targetHours);
+  const ratingDescription = getRatingDescription(rating);
+  const target = targetHours;
   const result = {
-    periodLength, trainingDays, average
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average
   };
-  console.log(result);
-  return dummyResult;
+  return result;
 }
 
 const inputArray = [3, 0, 2, 4.5, 0, 3, 1];
