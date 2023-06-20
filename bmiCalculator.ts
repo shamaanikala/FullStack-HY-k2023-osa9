1a0a50296c4793db0439c0b0a876e7aba9f1a7e0
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-06-20 13:15:12 +0300

    Ex9.1 basic functionality

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,5 +1,9 @@
 const calculateBmi = (height: number, weight: number): string =>  {
-  return 'Calculated BMI';
+  // height in cm, weight in kg
+  // BMI is calculated mass[kg]/height[m]^2
+  const heightInMeters = height/100;
+  const bmi = weight/(heightInMeters*heightInMeters)
+  return `Calculated BMI: ${bmi}`;
 }
 
-console.log(calculateBmi(180, 74));
+console.log(calculateBmi(180, 74)); // should return Normal (healthy weight)
