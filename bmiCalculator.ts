78b30ffcef9827daa466cc69c7893717116bd9d5
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-06-20 16:43:46 +0300

    9.1 sketch of a possible bmi type

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,8 +1,17 @@
-// toimisiko tällainen:
-type BMI = 'Underweight' | 'Normal' | 'Overweight' | 'Obese';
+interface BMI {
+  // Example:
+  // category: 'Normal'
+  // description: 'healthy weight'
+  // lowerBound: 18.5
+  // upperBound: 24.9
+  category: string;
+  description: string;
+  lowerBound?: number; // These are optional as the BMIs <= 16.0 or >= 40.0
+  upperBound?: number; // do not have lower or upper bounds.
+};
 
 const calculateBmi = (height: number, weight: number): string =>  {
-  // height in cm, weight in kg
+  // [height] = cm, [weight] = kg
   // BMI is calculated mass[kg]/height[m]^2
   const heightInMeters = height/100;
   const bmi = weight/(heightInMeters*heightInMeters)
