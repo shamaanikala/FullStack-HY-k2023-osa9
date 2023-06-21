166b0b32239a1fc0fec26f2dd72ff596e3a3c304
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-06-21 11:20:40 +0300

    9.3 argument parsing

--- utils/parseArgs.ts
+++ utils/parseArgs.ts
@@ -5,9 +5,32 @@ const getTSfileName = (arg: string): string => {
   return tsFile;
 }
 
+interface argsLengthResult {
+  success: boolean;
+  length: number;
+}
+
+const checkArgsLength = (args: string[], requiredLength: number): argsLengthResult => {
+  if (args && requiredLength) {
+    return args.length === requiredLength
+      ? { success: true, length: requiredLength }
+      : { success: false, length: args.length }
+  } else {
+    throw new Error('Bad arguments given!');
+  }
+}
+
 const parseArgs = (args: string[]) => {
+  const BMI_CALCULATOR_ARGS_LENGTH = 2;
+  // exerciseCalculator has variable length arguments
+  // but it must have a minimum amount of input arguments:
+  // target day1
+  const EXERCISE_CALCULATOR_ARGS_MIN_LENGTH = 2;
+
   const tsFile = getTSfileName(args[1]);
   // console.log(tsFile);
+  console.log(checkArgsLength([],3));
+  console.log(checkArgsLength(args,3));
   switch (tsFile) {
     case 'bmiCalculator.ts':
       return 'bmiCalculator.ts';
