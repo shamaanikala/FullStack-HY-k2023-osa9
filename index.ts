c757f6d50c1fd6c51f8d9359397241f23f8424fb
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-06-22 12:32:09 +0300

    9.5 dummyResponse for /bmi endpoint

--- index.ts
+++ index.ts
@@ -1,14 +1,34 @@
 import express from 'express';
+
+import calculateBmi from './bmiCalculator';
+
 const app = express();
+
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
 
+// interface BmiQuery {
+//   height: string;
+//   weight: string;
+// };
+
+// const parseBmiQuery = (query: BmiQuery) {
+  
+// }
+
 app.get('/bmi', (req, res) => {
   console.log('/bmi');
   console.log(req.query);
-  res.send('BMI-calculator');
+  const height = 180;
+  const weight = 72;
+  const bmiDummyResponse = {
+    weight,
+    height,
+    bmi: calculateBmi(height, weight)
+  };
+  res.json(bmiDummyResponse);
 });
 
 const PORT = 3002;
