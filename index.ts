4f7e5a5a706135320f381fe52dd0fe8f2cca682e
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-06-22 11:55:36 +0300

    Ex9.4 donEx9.4 done

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log('Hei!');
+import express from 'express';
+const app = express();
+
+app.get('/hello', (_req, res) => {
+  res.send('Hello Full Stack!');
+});
+
+const PORT = 3002;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
