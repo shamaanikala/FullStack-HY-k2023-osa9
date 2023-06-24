97ef9cecd0236fa108f8a080d6775527895e3fa0
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-06-24 16:39:12 +0300

    9.11 add Patient without SSN type

--- patientor-backend/src/services/patientsService.ts
+++ patientor-backend/src/services/patientsService.ts
@@ -1,10 +1,21 @@
 import patients from '../../data/patients';
-import { Patient } from '../types';
+import { Patient, PatientSansSSN } from '../types';
 
 const getPatients = () : Patient[] => {
   return patients;
 };
 
+const getPatientsSansSSN = () : PatientSansSSN[] => {
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation,
+  }));
+};
+
 export default {
   getPatients,
+  getPatientsSansSSN,
 };
