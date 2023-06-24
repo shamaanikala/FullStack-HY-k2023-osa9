e0b02bedbbb9e168a9adae1c91670ae0abc4c746
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-06-24 16:28:54 +0300

    9.11 base for patientsService

---
+++ patientor-backend/src/services/patientsService.ts
@@ -0,0 +1,11 @@
+import patients from '../../data/patients';
+
+import { Patient } from '../types';
+
+const getPatients = () : Patient[] => {
+  return patients;
+};
+
+export default {
+  getPatients,
+};
