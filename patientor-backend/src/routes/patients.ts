c5c0b2c581ceed0aec845fe344b23fbb0c5a0ee7
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-06-24 16:33:31 +0300

    9.11 patient route returs all data

--- patientor-backend/src/routes/patients.ts
+++ patientor-backend/src/routes/patients.ts
@@ -1,10 +1,11 @@
 import express from 'express';
+import patientsService from '../services/patientsService';
 
 const router = express.Router();
 
 router.get('/', (_req, res) => {
   console.log(`GET : ${_req.baseUrl}`);
-  res.send('Patient data...');
+  res.send(patientsService.getPatients());
 });
 
 export default router;
--- patientor-backend/src/services/patientsService.ts
+++ patientor-backend/src/services/patientsService.ts
@@ -1,5 +1,4 @@
 import patients from '../../data/patients';
-
 import { Patient } from '../types';
 
 const getPatients = () : Patient[] => {
