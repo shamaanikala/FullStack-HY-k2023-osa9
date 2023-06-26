d5e3bc4a3096dcbebe60c7d04e2fceb603f907ac
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-06-26 16:04:59 +0300

    Ex9.13 add parsing functions

--- patientor-backend/src/utlis/parseTools.ts
+++ patientor-backend/src/utlis/parseTools.ts
@@ -1,3 +1,11 @@
 export const isString = (text: unknown): text is string => {
   return typeof text === 'string' || text instanceof String;
 };
+
+export const parseStringParam = (fieldName: string, field: unknown): string => {
+  if (!isString(field)) {
+    throw new Error(`Incorrect or missing ${fieldName}`);
+  } 
+
+  return field;
+};
--- patientor-backend/src/utlis/patientTools.ts
+++ patientor-backend/src/utlis/patientTools.ts
@@ -1,7 +1,31 @@
 import { NewPatient } from "../types";
+import { isString, parseStringParam } from "./parseTools";
 
-const toNewPatient = (object: NewPatient): NewPatient => {
-// const toNewPatient = (object: unknown): NewPatient => {
+// Adding individual but currently redundant parsers for each field
+// as they could be done with just one function parsing valid string
+// Or let's at least add a parseStringParam to parseTools
+
+const parseName = (name: unknown): string => {
+  return parseStringParam('name', name);
+};
+
+const parseDateOfBirt = (dateOfBirth: unknown): string => {
+  return parseStringParam('dateOfBirth', dateOfBirth);
+};
+
+const parseSSN = (ssn: unknown): string => {
+  return parseStringParam('ssn', ssn);
+};
+
+const parseGender = (gender: unknown): string => {
+  return parseStringParam('gender', gender);
+};
+
+const parseOccupation = (occupation: unknown): string => {
+  return parseStringParam('occupation', occupation);
+};
+
+const toNewPatient = (object: unknown): NewPatient => {
   console.log(object);
   const newPatient: NewPatient = { ...object };
   return newPatient;
