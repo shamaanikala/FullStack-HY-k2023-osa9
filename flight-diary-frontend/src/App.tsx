0fe9233f1f348bb0025efd91ce8eb81a74e86010
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-06-27 19:24:35 +0300

    9.16: Backend configuration:
    Backend:
    - npm install cors
    - npm install --save-dev @types/cors
    - add cors to index.ts
    Frontend:
    - install axios
    - basic axios.get to useEffect

--- flight-diary-frontend/src/App.tsx
+++ flight-diary-frontend/src/App.tsx
@@ -1,4 +1,16 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const App = () => {
+  const [entries, setEntries] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/api/diaries')
+      .then(response => {
+        console.log(response.data);
+      });
+  }, []);
+
   return (
     <div>
       <h2>Flight Diary</h2>
