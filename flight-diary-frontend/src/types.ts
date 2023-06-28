bbc9dc07fe2d57241560cbb183ebbf6aba3112d9
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-06-28 20:18:26 +0300

    9.19 copy-paste Weather&Visibility from backend

--- flight-diary-frontend/src/types.ts
+++ flight-diary-frontend/src/types.ts
@@ -18,3 +18,18 @@ export interface NewDiaryEntryFormProps {
 export interface DiaryEntryFormErrorMessageProps {
   message: string;
 }
+
+export enum Weather {
+  Sunny = 'sunny',
+  Rainy = 'rainy',
+  Cloudy = 'cloudy',
+  Stormy = 'stormy',
+  Windy = 'windy',
+}
+
+export enum Visibility {
+  Great = 'great',
+  Good = 'good',
+  Ok = 'ok',
+  Poor = 'poor',
+}
