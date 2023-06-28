d9aa181d036e1daff14744bf2b7149e2d2118d87
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-06-28 12:13:52 +0300

    9.17 empty form

--- flight-diary-frontend/src/components/DiaryEntryForm.tsx
+++ flight-diary-frontend/src/components/DiaryEntryForm.tsx
@@ -1,6 +1,24 @@
 const DiaryEntryForm = () => {
   return (
     <div>
+      <h2>Add new entry</h2>
+      <div>
+        <form>
+          <div>
+            date <input />
+          </div>
+          <div>
+            visibility <input />
+          </div>
+          <div>
+            weather <input />
+          </div>
+          <div>
+            comment <input />
+          </div>
+          <button type="submit">add</button>
+        </form>
+      </div>
       <h5>Diary entry form here</h5>
     </div>
   );
