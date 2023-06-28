98d90dd89d77db812610a3a69a16eec4c9ec9742
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-06-28 12:09:04 +0300

    9.17 <DiaryEntryForm> to App.tsx

--- flight-diary-frontend/src/App.tsx
+++ flight-diary-frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import DiaryEntry from "./components/DiaryEntry";
 import { NonSensitiveDiaryEntry } from "../../flight-diary/src/types";
 import { getAllDiaryEntries } from "./services/diaryService";
+import DiaryEntryForm from "./components/DiaryEntryForm";
 
 const App = () => {
   const [entries, setEntries] = useState<NonSensitiveDiaryEntry[]>([]);
@@ -13,6 +14,9 @@ const App = () => {
   return (
     <div>
       <h2>Flight Diary</h2>
+      <div>
+        <DiaryEntryForm />
+      </div>
       <div>
         <h2>Diary entries</h2>
         {entries.map(entry => <DiaryEntry key={entry.id} entry={entry} />)}
