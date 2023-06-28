b4a8a856c44a5895d5c449b8b578aa93be4ad5fa
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-06-28 14:50:40 +0300

    9.17 createDiaryEntry

--- flight-diary-frontend/src/services/diaryService.tsx
+++ flight-diary-frontend/src/services/diaryService.tsx
@@ -1,9 +1,15 @@
 import axios from 'axios';
-import { NonSensitiveDiaryEntry } from "../../../flight-diary/src/types";
+import { NewDiaryEntry, NonSensitiveDiaryEntry } from "../../../flight-diary/src/types";
 
 const baseUrl = 'http://localhost:3001/api/diaries';
 
 export const getAllDiaryEntries = () => {
   return axios.get<NonSensitiveDiaryEntry[]>(baseUrl)
+    .then(response => response.data);
+};
+
+export const createDiaryEntry = (object: NewDiaryEntry) => {
+  return axios
+    .post<NewDiaryEntry>(baseUrl, object)
     .then(response => response.data);
 };
