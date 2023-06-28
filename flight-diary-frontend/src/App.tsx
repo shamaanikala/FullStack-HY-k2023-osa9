995495814b938b723f4629c468b979fbde0113b1
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-06-28 11:56:30 +0300

    9.16 App.tsx renders the diary entries

--- flight-diary-frontend/src/App.tsx
+++ flight-diary-frontend/src/App.tsx
@@ -1,21 +1,25 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import DiaryEntry from "./components/DiaryEntry";
+import { NonSensitiveDiaryEntry } from "../../flight-diary/src/types";
 
 const App = () => {
-  const [entries, setEntries] = useState([]);
+  const [entries, setEntries] = useState<NonSensitiveDiaryEntry[]>([]);
 
   useEffect(() => {
-    axios.get('http://localhost:3001/api/diaries')
+    axios.get<NonSensitiveDiaryEntry[]>('http://localhost:3001/api/diaries')
       .then(response => {
-        console.log(response.data);
+        setEntries(response.data);
       });
   }, []);
 
   return (
     <div>
       <h2>Flight Diary</h2>
-      <DiaryEntry />
+      <div>
+        <h2>Diary entries</h2>
+        {entries.map(entry => <DiaryEntry key={entry.id} entry={entry} />)}
+      </div>
     </div>
   );
 };
