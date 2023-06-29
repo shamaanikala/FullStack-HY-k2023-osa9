3a4c55337a8102a44a8977d24d883673e769a9e6
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-06-29 15:18:46 +0300

    9.19 refactor <VisibilityRadioButtons> to own file

---
+++ flight-diary-frontend/src/components/VisibilityRadioButtons.tsx
@@ -0,0 +1,25 @@
+import { Visibility, VisibilityRadioButtonsProps } from "../types";
+import { getEnumValues } from "../utils";
+
+const VisibilityRadioButtons = (props: VisibilityRadioButtonsProps) => {
+    const name = 'visibility';
+    const enumObject = Visibility;
+    const values = getEnumValues(enumObject); 
+    // console.log(values)
+    return (
+      <>
+        {values.map(v =>
+          <span key={v}>
+            <label key={`${v}-label`} htmlFor={v}>{v}</label>
+            <input
+              type="radio"
+              id={v}
+              name={name}
+              onChange={() => props.handleVisibility(v)} />
+          </span>
+        )}
+      </>
+    );
+  };
+
+  export default VisibilityRadioButtons;
