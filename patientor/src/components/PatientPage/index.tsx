c27184c556977e1b6c3203bf6f7aa6fe7313dc0e
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-06-30 11:20:18 +0300

    9.21 render patient name on the patient page

--- patientor/src/components/PatientPage/index.tsx
+++ patientor/src/components/PatientPage/index.tsx
@@ -1,19 +1,44 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Patient } from "../../types";
+import patientService from '../../services/patients';
 
-interface Props {
-  patient : Patient;
-  setPatient: React.Dispatch<React.SetStateAction<Patient>>
-}
-
-// Ex9.21 patiet as prop from PatietnListPage?
-// Or just send id as prop and fetch here byId
 // or no props and use React Router useParams
 const PatientPage = () => {
   console.log(useParams().id);
+  const id = useParams().id;
+  const [patient, setPatient] = useState<Patient>();
+
+  const getPatientInformation = async (id: string) => {
+    const response = await patientService.getOneById(id);
+    return response;
+  };
+
+  useEffect(() => {
+    const fetchData = async (id: string) => {
+      try {
+        if (id) {
+          const response = await getPatientInformation(id);
+          setPatient(response);
+        } else {
+          throw new Error('Missing patient id!');
+        }
+      } catch (error) {
+        if (error instanceof Error) {
+          console.error(error.message);
+        } else {
+          console.error(error);
+        }
+      }
+    };
+    if (id) {
+      fetchData(id);
+    }
+  },[id]);
+
   return (
     <div>
-      <i>Patient information here</i>
+      {patient && <h1>{patient.name}</h1>}
     </div>
   );
 };
