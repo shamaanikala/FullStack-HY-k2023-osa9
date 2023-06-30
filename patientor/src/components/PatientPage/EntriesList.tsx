3aa5d5be72a82f108e6a0111893c2ad45cbda372
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-06-30 19:39:22 +0300

    9.23 building the entries list

--- patientor/src/components/PatientPage/EntriesList.tsx
+++ patientor/src/components/PatientPage/EntriesList.tsx
@@ -7,9 +7,12 @@ interface Props {
 
 const EntriesList = (props: Props) => {
   console.log(props.entries);
+  const entries = props.entries;
   return (
     <List subheader={<ListSubheader><h3>entries</h3></ListSubheader>}>
-
+      {entries.map(e =>
+        <ListSubheader>{e.date}</ListSubheader>
+      )}
     </List>
   );
 };
