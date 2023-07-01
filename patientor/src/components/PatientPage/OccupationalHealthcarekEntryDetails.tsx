c01b930ec36f92f3e8c2ef74b5bf08a76e67e7c7
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-01 15:52:17 +0300

    9.25 occupational entry detail header

--- patientor/src/components/PatientPage/OccupationalHealthcarekEntryDetails.tsx
+++ patientor/src/components/PatientPage/OccupationalHealthcarekEntryDetails.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Badge, Card, CardHeader, ListItemText } from "@mui/material"
+import { Avatar, Badge, Card, CardHeader, ListItemText, Typography } from "@mui/material"
 import { Diagnosis, OccupationalHealthcareEntry } from "../../types";
 import MedicalInformationIcon from '@mui/icons-material/MedicalInformation';
 import BadgeIcon from '@mui/icons-material/Badge';
@@ -15,16 +15,29 @@ const OccupationalHealthcareEntryDetails = (props: OccupationalEntryProps) => {
   const diagnoses = props.diagnoses;
   const getDiagnosisName = props.getDiagnosisName;
 
+  interface EmployerProp {
+    name: string;
+  }
+  const EmployerIcon = ({ name }: EmployerProp) => {
+    return (
+      <Typography title="Employer">
+        <Badge><WorkOutlineIcon /></Badge>
+        <span>{" "}<small>{name}</small></span>
+      </Typography>
+    );
+  };
+
   return (
     <Card>
       <CardHeader
         avatar={
-          <Badge color="primary" badgeContent={'Occupational'}>
+          <Badge color="primary">
           <Avatar title="Occupational healthcare">
             <MedicalInformationIcon />
           </Avatar>
           </Badge>}
-        title={`${e.date} - ${e.employerName} `}
+        title={e.date}
+        subheader={<EmployerIcon name={e.employerName} />}
       />
       <ListItemText><i>{e.description}</i></ListItemText>
               {e.diagnosisCodes && <div><ul>
