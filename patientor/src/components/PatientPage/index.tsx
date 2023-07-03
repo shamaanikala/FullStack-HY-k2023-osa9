ab66878511912e843d76bb56a7001c69a291c4ad
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-03 11:40:50 +0300

    9.27 add modal that opens dummy form component

--- patientor/src/components/PatientPage/index.tsx
+++ patientor/src/components/PatientPage/index.tsx
@@ -1,20 +1,54 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Patient } from "../../types";
+import { EntryFormValues, Patient } from "../../types";
 import patientService from '../../services/patients';
 
 import FemaleIcon from '@mui/icons-material/Female';
 import MaleIcon from '@mui/icons-material/Male';
-import { Card, CardContent } from "@mui/material";
+import { Button, Card, CardContent } from "@mui/material";
 import EntriesList from "./EntriesList";
 
 import { calculateAge } from "../../utils";
+import AddEntryModal from "../AddEntryModal";
 
 // or no props and use React Router useParams
 const PatientPage = () => {
   console.log(useParams().id);
   const id = useParams().id;
   const [patient, setPatient] = useState<Patient>();
+
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [error, setError] = useState<string>();
+
+  const openModal = (): void => setModalOpen(true);
+
+  const closeModal = (): void => {
+    setModalOpen(false);
+    setError(undefined);
+  };
+
+  const submitNewEntry = async (values: EntryFormValues) => {
+    console.log('submitNewEntry');
+    console.log(values);
+    // try {
+    //   const patient = await patientService.create(values);
+    //   setPatients(patients.concat(patient));
+    //   setModalOpen(false);
+    // } catch (e: unknown) {
+    //   if (axios.isAxiosError(e)) {
+    //     if (e?.response?.data && typeof e?.response?.data === "string") {
+    //       const message = e.response.data.replace('Something went wrong. Error: ', '');
+    //       console.error(message);
+    //       setError(message);
+    //     } else {
+    //       setError("Unrecognized axios error");
+    //     }
+    //   } else {
+    //     console.error("Unknown error", e);
+    //     setError("Unknown error");
+    //   }
+    // }
+  };
 
   const getPatientInformation = async (id: string) => {
     const response = await patientService.getOneById(id);
@@ -48,29 +82,42 @@ const PatientPage = () => {
   }
 
   return (
-    <Card>
-      <CardContent>
-        <span>
-        <h1>
-          {patient.name}{" "}
-          {patient.gender === 'male' &&  <span title="male"><MaleIcon fontSize="small" /></span>}
-          {patient.gender === 'female' && <span title="female"><FemaleIcon fontSize="small" /></span>}
-          {patient.gender === 'other' && <span title="gender: other"><small><i>other</i></small></span>}
-        </h1></span>
-        <div title={`Exact age: ${parseFloat(calculateAge(patient.dateOfBirth).toString()).toFixed(2)}`}>
-          age: {Math.floor(calculateAge(patient.dateOfBirth))}
-        </div>
-        <div>
-          ssn: {patient.ssn}
-        </div>
-        <div>
-          occupation: {patient.occupation}
-        </div>
-        <div>
-          <EntriesList entries={patient.entries} />
-        </div>
-      </CardContent>
-    </Card>
+    <div>
+      <div>
+        <AddEntryModal
+          modalOpen={modalOpen}
+          onSubmit={submitNewEntry}
+          error={error}
+          onClose={closeModal}
+        />
+        <Button variant="contained" onClick={() => openModal()}>
+          Add New Patient
+        </Button>
+      </div>
+      <Card>
+        <CardContent>
+          <span>
+          <h1>
+            {patient.name}{" "}
+            {patient.gender === 'male' &&  <span title="male"><MaleIcon fontSize="small" /></span>}
+            {patient.gender === 'female' && <span title="female"><FemaleIcon fontSize="small" /></span>}
+            {patient.gender === 'other' && <span title="gender: other"><small><i>other</i></small></span>}
+          </h1></span>
+          <div title={`Exact age: ${parseFloat(calculateAge(patient.dateOfBirth).toString()).toFixed(2)}`}>
+            age: {Math.floor(calculateAge(patient.dateOfBirth))}
+          </div>
+          <div>
+            ssn: {patient.ssn}
+          </div>
+          <div>
+            occupation: {patient.occupation}
+          </div>
+          <div>
+            <EntriesList entries={patient.entries} />
+          </div>
+        </CardContent>
+      </Card>
+    </div>
   );
 };
 
