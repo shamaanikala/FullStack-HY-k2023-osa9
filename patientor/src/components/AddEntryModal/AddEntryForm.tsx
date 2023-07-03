cb1b1ef393849a7f0cab877c187f61301d9bd69b
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-03 14:17:35 +0300

    9.27 adding some dummy functionality

--- patientor/src/components/AddEntryModal/AddEntryForm.tsx
+++ patientor/src/components/AddEntryModal/AddEntryForm.tsx
@@ -1,4 +1,6 @@
+import { Card, TextField } from "@mui/material";
 import { EntryFormValues } from "../../types";
+import { SyntheticEvent, useState } from "react";
 
 interface Props {
   onCancel: () => void;
@@ -6,8 +8,28 @@ interface Props {
 }
 
 const AddEntryForm = ({ onCancel, onSubmit }: Props) => {
+  const [description, setDescription] = useState('');
+
+  const addEntry = (event: SyntheticEvent) => {
+    event.preventDefault();
+    console.log('Submitting add entry form');
+    console.log(event);
+  };
+
   return (
-    <div>Add entry form here</div>
+    <div>
+      <Card>
+        <form onSubmit={addEntry}>
+          <TextField
+            label="Description"
+            placeholder="Type entry description here.."
+            fullWidth
+            value={description}
+            onChange={({ target }) => setDescription(target.value)}
+          />
+        </form>
+      </Card>
+    </div>
   );
 };
 
