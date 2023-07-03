b8f2a99b8eebd922b0b9142e74b3cc417276a55e
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-03 11:25:42 +0300

    9.27 basis for AddEntryForm
    Based on the existing addPatient modality

--- patientor/src/components/AddEntryModal/AddEntryForm.tsx
+++ patientor/src/components/AddEntryModal/AddEntryForm.tsx
@@ -1,4 +1,11 @@
-const AddEntryForm = () => {
+import { EntryFormValues } from "../../types";
+
+interface Props {
+  onCancel: () => void;
+  onSubmit: (values: EntryFormValues) => void;
+}
+
+const AddEntryForm = ({ onCancel, onSubmit }: Props) => {
   return (
     <div>Add entry form here</div>
   );
--- patientor/src/components/AddEntryModal/index.tsx
+++ patientor/src/components/AddEntryModal/index.tsx
@@ -1,7 +1,27 @@
-const AddEntryModal = () => {
-  return (
-  <div>AddPatientModal</div>
-  );
-};
+import Dialog from "@mui/material/Dialog/Dialog";
+import { EntryFormValues } from "../../types";
+import DialogTitle from "@mui/material/DialogTitle/DialogTitle";
+import Divider from "@mui/material/Divider/Divider";
+import DialogContent from "@mui/material/DialogContent/DialogContent";
+import AddEntryForm from "./AddEntryForm";
+import Alert from "@mui/material/Alert/Alert";
+
+interface Props {
+  modalOpen: boolean;
+  onClose: () => void;
+  onSubmit: (values: EntryFormValues) => void;
+  error?: string;
+}
+
+const AddEntryModal = ({ modalOpen, onClose, onSubmit, error }: Props) => (
+  <Dialog fullWidth={true} open={modalOpen} onClose={() => onClose()}>
+    <DialogTitle>Add a new patient</DialogTitle>
+    <Divider />
+    <DialogContent>
+      {error && <Alert severity="error">{error}</Alert>}
+      <AddEntryForm onSubmit={onSubmit} onCancel={onClose}/>
+    </DialogContent>
+  </Dialog>
+);
 
 export default AddEntryModal;
