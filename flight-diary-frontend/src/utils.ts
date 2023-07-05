fd431a7e01129884dbd308cdb336541ffb80c1c1
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-05 16:49:29 +0300

    Ex9.19 fix the solution
    No more explicit any

--- flight-diary-frontend/src/utils.ts
+++ flight-diary-frontend/src/utils.ts
@@ -1,20 +1,8 @@
-// accroding to 
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Enumerability_and_ownership_of_properties#traversing_object_properties
-// for ... in should be possible with Enum
-// for (const vis in Visibility) {
-//   console.log(vis); // works fine!
-// }
-
-// eslint dispable any, I know what I'm doing
-// ... and I have no idea how to do this without the any
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const getEnumValues = (enumObject: any): string[] => {
-  const values = [];
-  // for ... in loops throught the keys...
-  for (const val in enumObject) {
-    // some one must get the value with 
-    // console.log(enumObject[val]);
-    values.push(enumObject[val]);
+export const getEnumValues = (enumObject: unknown): string[] => {
+  if(!enumObject || typeof enumObject !== 'object') {
+    throw new Error('Invalid enumObject');
   }
+
+  const values: string[] = Object.values(enumObject);
   return values;
-};
+}
